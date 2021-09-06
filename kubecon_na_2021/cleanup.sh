387f83ae16837a81db37a48de4edbeb013f00ff0
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2021-09-06 11:59:54 +0000

    update
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

---
+++ kubecon_na_2021/cleanup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export KUBECONFIG=$HOME/admin.conf
+
+kubectl delete -f cluster-on-pvc.yaml
+
+for i in operator common crds ; do
+  kubectl delete -f ${i}.yaml
+done
+
+kubectl delete -f local-pvs.yaml
+
+sudo rm -rf /var/lib/rook
+
+for DISK in /dev/sdb /dev/sdc ; do
+  sudo sgdisk --zap-all ${DISK}
+  sudo dd if=/dev/zero of=${DISK} bs=1M count=100 oflag=dsync,direct
+  sudo blkdiscard ${DISK}
+done
+
+ls /dev/mapper/ceph-* | xargs -I% -- dmsetup remove %
+
+sudo rm -rf /dev/mapper/ceph-*
